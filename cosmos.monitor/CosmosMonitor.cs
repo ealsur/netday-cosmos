0fe0b8154a839e65c45fd86eb7c6d40f897da127
Matias Quaranta <maquaran@microsoft.com>
2023-03-15 14:01:24 -0700

    Base layout

---
+++ cosmos.monitor/CosmosMonitor.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace cosmos.monitor
+{
+    public class CosmosMonitor
+    {
+        private ChangeFeedEstimator estimator; 
+
+        /// <summary>
+        /// Creates an Estimator to estimate the Processor progress
+        /// </summary>
+        private ChangeFeedEstimator GetOrCreateEstimator(CosmosClient cosmosClient)
+        {
+            this.estimator ??= cosmosClient.GetContainer(Environment.GetEnvironmentVariable("DATABASE"), Environment.GetEnvironmentVariable("EVENTCONTAINER")).GetChangeFeedEstimator("EventProcessor", cosmosClient.GetContainer(Environment.GetEnvironmentVariable("DATABASE"), "leases"));
+
+            return this.estimator;
+        }
+
+        [FunctionName("CosmosMonitor")]
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo _,
+            [CosmosDB(
+            databaseName: "%DATABASE%",
+            containerName: "%EVENTCONTAINER%",
+            PreferredLocations = "%REGION%",
+            Connection = "cosmosConnection")]CosmosClient cosmosClient,
+            ILogger log)
+        {
+            var estimator = GetOrCreateEstimator(cosmosClient).GetCurrentStateIterator();
+            while (estimator.HasMoreResults)
+            {
+                var response = await estimator.ReadNextAsync();
+                foreach (var item in response)
+                {
+                    log.LogMetric($"Lease {item.LeaseToken}", item.EstimatedLag, new Dictionary<string, object>() { { "Owner", item.InstanceName } });
+                }
+            }
+        }
+    }
+}
---
+++ cosmos.trigger/CosmosProcessor.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace cosmos.trigger
+{
+    public static class CosmosProcessor
+    {
+        // Support for retries
+        [FixedDelayRetry(3, "00:00:10")]
+        [FunctionName("CosmosProcessor")]
+        public static async Task Run(
+            // Trigger to react to events
+            [CosmosDBTrigger(
+            databaseName: "%DATABASE%",
+            containerName: "%EVENTCONTAINER%",
+            Connection = "cosmosConnection",
+            LeaseContainerName = "leases",
+            PreferredLocations = "%REGION%",
+            MaxItemsPerInvocation = 1000,
+            LeaseContainerPrefix = "EventProcessor")]IReadOnlyList<DeviceTelemetry> events,
+
+            // Output binding to generate summary
+            [CosmosDB(
+            databaseName: "%DATABASE%",
+            containerName: "%SUMMARYCONTAINER%",
+            Connection = "cosmosConnection")]IAsyncCollector<Summary> summary,
+
+            ILogger log)
+        {
+            foreach (var group in events.GroupBy(singleEvent => singleEvent.DeviceId))
+            {
+                log.LogInformation($"Generating summary for device {group.Key}...");
+                await summary.AddAsync(new Summary()
+                {
+                    id = Guid.NewGuid().ToString(),
+                    DeviceId = group.Key,
+                    Time = DateTime.UtcNow.ToString("s"),
+                    MaxValue = group.Max(item => item.Value)
+                });
+            }
+        }
+
+
+        public class DeviceTelemetry
+        {
+            public string DeviceId { get; set; }
+
+            public string Value { get; set; }
+        }
+
+        public class Summary
+        {
+            public string id { get; set; }
+
+            public string DeviceId { get; set; }
+
+            public string Time { get; set; }
+
+            public string MaxValue { get; set; }
+        }
+    }
+}
