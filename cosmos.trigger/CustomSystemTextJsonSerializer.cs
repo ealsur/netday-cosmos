7beee296c63716e4ee25f0d1c21ba09ae64e4be5
Matias Quaranta <ealsur@hotmail.com>
2023-03-24 14:36:13 -0700

    Adding dashboard and custom serializer

---
+++ cosmos.trigger/CustomSystemTextJsonSerializer.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Core.Serialization;
+using Microsoft.Azure.Cosmos;
+using System.IO;
+using System.Text.Json;
+
+namespace cosmos.trigger
+{
+    public class CustomSystemTextJsonSerializer : CosmosSerializer
+    {
+        private readonly JsonObjectSerializer systemTextJsonSerializer;
+
+        public CustomSystemTextJsonSerializer(JsonSerializerOptions jsonSerializerOptions)
+        {
+            this.systemTextJsonSerializer = new JsonObjectSerializer(jsonSerializerOptions);
+        }
+
+        public override T FromStream<T>(Stream stream)
+        {
+            using (stream)
+            {
+                if (stream.CanSeek
+                       && stream.Length == 0)
+                {
+                    return default;
+                }
+
+                if (typeof(Stream).IsAssignableFrom(typeof(T)))
+                {
+                    return (T)(object)stream;
+                }
+
+                return (T)this.systemTextJsonSerializer.Deserialize(stream, typeof(T), default);
+            }
+        }
+
+        public override Stream ToStream<T>(T input)
+        {
+            MemoryStream streamPayload = new MemoryStream();
+            this.systemTextJsonSerializer.Serialize(streamPayload, input, typeof(T), default);
+            streamPayload.Position = 0;
+            return streamPayload;
+        }
+    }
+}
---
+++ cosmos.trigger/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+[assembly: FunctionsStartup(typeof(cosmos.trigger.Startup))]
+namespace cosmos.trigger
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<ICosmosDBSerializerFactory, MyCosmosDBSerializerFactory>();
+        }
+    }
+
+    public class MyCosmosDBSerializerFactory : ICosmosDBSerializerFactory
+    {
+        public CosmosSerializer CreateSerializer()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                // Other System.Text.Json custom confits
+            };
+
+            return new CustomSystemTextJsonSerializer(options);
+        }
+    }
+}
